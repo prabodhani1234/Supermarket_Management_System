f55df7a68154a6e234a1045ac31a91025c791a12
prabodhani1234 <96753@fhss.sjp.ac.lk>
2021-07-12 21:55:21 -0700

    Database connecting

--- SuperMarket/src/Test/DatabaseConnecting.java
+++ SuperMarket/src/Test/DatabaseConnecting.java
@@ -1,15 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 
-/**
- *
- * @author User
- */
+
+
 public class DatabaseConnecting {
-    
+    static Connection conection(){
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con=(Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/supermarketsystem","root","");
+            return con;
+        } catch (Exception e) {
+            System.out.println(e);
+            return null;
+        }
+    }
 }
