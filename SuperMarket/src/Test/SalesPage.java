7b339a24a7a430a658ae1a94cb26f8dba3318bee
prabodhani1234 <96753@fhss.sjp.ac.lk>
2021-07-15 08:09:12 -0700

    adding date to the sales page

--- SuperMarket/src/Test/SalesPage.java
+++ SuperMarket/src/Test/SalesPage.java
@@ -5,6 +5,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import javax.swing.table.DefaultTableModel;
 import net.proteanit.sql.DbUtils;
 
@@ -20,7 +22,7 @@ public class SalesPage extends javax.swing.JFrame {
         initComponents();
         con=DatabaseConnecting.conection();
         dataLoad();
-        
+        date();
     }
 
     /**
@@ -67,6 +69,7 @@ public class SalesPage extends javax.swing.JFrame {
         print = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
+        dateL = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setUndecorated(true);
@@ -404,18 +407,23 @@ public class SalesPage extends javax.swing.JFrame {
         jLabel1.setForeground(new java.awt.Color(255, 255, 255));
         jLabel1.setText("SUPERMARKET MANAGEMANT SYSTEM");
 
+        dateL.setFont(new java.awt.Font("Tahoma", 1, 18)); // NOI18N
+        dateL.setForeground(new java.awt.Color(255, 255, 255));
+        dateL.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(303, 303, 303)
-                .addComponent(jLabel1)
-                .addGap(0, 0, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(24, 24, 24)
-                .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 114, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(303, 303, 303)
+                        .addComponent(jLabel1)
+                        .addGap(165, 165, 165)
+                        .addComponent(dateL, javax.swing.GroupLayout.PREFERRED_SIZE, 181, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(25, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -425,7 +433,10 @@ public class SalesPage extends javax.swing.JFrame {
                     .addComponent(jButton1)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(24, 24, 24)
-                        .addComponent(jLabel1)))
+                        .addComponent(jLabel1))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(dateL, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(27, Short.MAX_VALUE))
@@ -445,6 +456,13 @@ public class SalesPage extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    
+    public void date(){
+       DateTimeFormatter date=DateTimeFormatter.ofPattern("yyyy-mm-dd");
+       LocalDateTime now=LocalDateTime.now();
+       dateL.setText(date.format(now));
+    }
+    
     public void searchProduct(){
         
         String search=searchbox.getText();
@@ -643,6 +661,7 @@ public class SalesPage extends javax.swing.JFrame {
     private javax.swing.JButton add;
     private javax.swing.JTextField balancebox;
     private javax.swing.JTextArea billarea;
+    private javax.swing.JLabel dateL;
     private javax.swing.JButton displatbill;
     private javax.swing.JTextField idbox;
     private javax.swing.JButton jButton1;
