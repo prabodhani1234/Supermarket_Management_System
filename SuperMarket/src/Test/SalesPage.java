8919af06483734b0c3e45fdf2a88da9cb5fa79e1
prabodhani1234 <96753@fhss.sjp.ac.lk>
2021-07-16 09:47:45 -0700

    setting sales page center of the screen

--- SuperMarket/src/Test/SalesPage.java
+++ SuperMarket/src/Test/SalesPage.java
@@ -1,6 +1,8 @@
 
 package Test;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -20,6 +22,11 @@ public class SalesPage extends javax.swing.JFrame {
     
     public SalesPage() {
         initComponents();
+        //set center
+        Toolkit toolkit =getToolkit();
+        Dimension size =toolkit.getScreenSize();
+        setLocation(size.width/2 - getWidth()/2, size.height/2 - getHeight()/2);
+        
         con=DatabaseConnecting.conection();
         dataLoad();
         date();
