37818d2dc0a1a3fe6e5c0794b1b5ea5dedaf3948
prabodhani1234 <96753@fhss.sjp.ac.lk>
2021-07-17 00:53:51 -0700

    database connect to recovery password page

--- SuperMarket/src/Test/PasswordRecovery.java
+++ SuperMarket/src/Test/PasswordRecovery.java
@@ -1,21 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Test;
 
-/**
- *
- * @author User
- */
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
 public class PasswordRecovery extends javax.swing.JFrame {
 
-    /**
-     * Creates new form PasswordRecovery
-     */
+    Connection con=null;
+    Statement stmt=null;
+    
+    
     public PasswordRecovery() {
         initComponents();
+        con=DatabaseConnecting.conection();
     }
 
     /**
